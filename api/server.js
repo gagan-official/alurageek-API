d26ca39d9fba6d8c0b3f1a9bc5bafe2e3410135a
Gagandeep Singh <94315969+gagan-official@users.noreply.github.com>
2024-05-18 20:35:02 +0530

    Updated server.js

--- api/server.js
+++ api/server.js
@@ -1,29 +1,35 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const cors = require('cors')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const cors = require('cors');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/product/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-
+// CORS configuration
 const corsOptions = {
-    origin: 'https://www.listenit-music-player.netlify.app',
-    methods: '*',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+  origin: 'https://www.listenit-music-player.netlify.app',
+  methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Date', 'X-Api-Version'],
+  credentials: true,
 };
+
+// Use middlewares
+server.use(middlewares);
+
+// Add CORS middleware
 server.use(cors(corsOptions));
 
+// Rewriter middleware
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+  '/product/:resource/:id/show': '/:resource/:id'
+}));
+
+// Use router middleware
+server.use(router);
+
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
